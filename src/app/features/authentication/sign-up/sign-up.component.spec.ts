33266af35d5305bbb810de4639a64d331dfa5ca5
velidduranovic <velidprivate@gmail.com>
2023-11-22 04:57:57 +0100

    Added unit tests for signup form

--- src/app/features/authentication/sign-up/sign-up.component.spec.ts
+++ src/app/features/authentication/sign-up/sign-up.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SignUpComponent } from './sign-up.component';
+import { ReactiveFormsModule } from "@angular/forms";
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
@@ -7,7 +8,8 @@ describe('SignUpComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SignUpComponent],
+      imports: [ReactiveFormsModule],
+      declarations: [SignUpComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SignUpComponent);
@@ -15,7 +17,63 @@ describe('SignUpComponent', () => {
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should require first name', () => {
+    const firstName = component.form.get('firstName');
+    firstName?.setValue('');
+    expect(firstName?.valid).toBeFalsy();
+  });
+
+  it('should require last name', () => {
+    const lastName = component.form.get('lastName');
+    lastName?.setValue('');
+    expect(lastName?.valid).toBeFalsy();
+  });
+
+  it('should require a valid email', () => {
+    const email = component.form.get('email');
+    email?.setValue('invalidemail'); // Set an invalid email
+    expect(email?.valid).toBeFalsy();
+  });
+
+  it('should require a valid password', () => {
+    const password = component.form.get('password');
+    password?.setValue('weak'); // Set a weak password
+    expect(password?.valid).toBeFalsy();
+  });
+
+  it('should not allow password to contain first name', () => {
+    const firstName = component.form.get('firstName');
+    firstName?.setValue("Velid");
+
+    const password = component.form.get('password');
+    password?.setValue('Password123' + firstName?.value); // Set password containing first name
+    expect(password?.hasError('noFirstNameLastName')).toBeTruthy();
+  });
+
+  it('should not allow password to contain last name', () => {
+    const lastName = component.form.get('lastName');
+    lastName?.setValue("Duranovic");
+
+    const password = component.form.get('password');
+    password?.setValue('Password123' + lastName?.value); // Set password containing first name
+    expect(password?.hasError('noFirstNameLastName')).toBeTruthy();
+  });
+
+  // Happy path
+
+  it('should have a valid form', ()=>{
+    const form = component.form;
+    form.setValue({
+      firstName: 'Velid',
+      lastName: 'Duranovic',
+      email: 'validemail@mymail.com',
+      password: 'MyPassword123',
+    });
+    expect(form.valid).toBeTruthy();
+  })
 });
